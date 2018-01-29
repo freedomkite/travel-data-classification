461ce4f8bd86266d5df01162153698a5149b9f61
walker-liu <1499964186@qq.com>
2018-01-29 17:55:15 +0800

    update model and add test data

--- lstm.py
+++ lstm.py
@@ -1,17 +1,19 @@
-#encoding:utf-8
+# encoding:utf-8
 import torch
-import  torch.nn as nn
+import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import  Variable
+from torch.autograd import Variable
+
 torch.manual_seed(1)
 
+
 class BiLSTM(nn.Module):
-    def __init__(self,args):
-        super(BiLSTM,self).__init__()
+    def __init__(self, args):
+        super(BiLSTM, self).__init__()
         # self.args=args
         # self.embed=nn.Embedding(args.embed_num, args.embed_dim)
         # self.pos_embedding = nn.Embedding(args.pos_size, args.pos_dim)
-        #self.dropout = nn.Dropout(args.dropout)
+        # self.dropout = nn.Dropout(args.dropout)
         #
         # self.lstm = nn.LSTM(args.input_size, args.hidden_size,
         #                     bidirectional=False,
@@ -23,16 +25,17 @@ class BiLSTM(nn.Module):
         # self.softmax = nn.Softmax()
         self.args = args
         self.embed = nn.Embedding(args.embed_num, args.embed_dim)
-        #self.dropout = nn.Dropout(args.dropout)
+        self.dropout = nn.Dropout(args.dropout)
         self.lstm = nn.LSTM(args.embed_dim, args.hidden_size,
                             bidirectional=True,
                             batch_first=True,
                             dropout=args.dropout)
         # dropout=args.dropout_rnn)
         # self.myw = Variable(torch.randn(args.hidden_size * 2,args.hidden_size*2), requires_grad=True).cuda()
-        self.linearOut = nn.Linear(args.hidden_size*2, args.class_num)
+        self.linearOut = nn.Linear(args.hidden_size * 2, args.class_num)
         self.softmax = nn.Softmax()
-    def forward(self,x,):
+
+    def forward(self, x, ):
         if torch.cuda.is_available():
             hidden = (Variable(torch.zeros(2, x.size(0), self.args.hidden_size)).cuda(),
                       Variable(torch.zeros(2, x.size(0), self.args.hidden_size)).cuda())
@@ -40,7 +43,7 @@ class BiLSTM(nn.Module):
             hidden = (Variable(torch.zeros(2, x.size(0), self.args.hidden_size)),
                       Variable(torch.zeros(2, x.size(0), self.args.hidden_size)))
         x = self.embed(x)
-        #x=self.dropout(x)
+
         x, lstm_h = self.lstm(x, hidden)
         x = F.tanh(x)
         # for idx in range(x.size(0)):
@@ -49,11 +52,12 @@ class BiLSTM(nn.Module):
         #         output = torch.unsqueeze(h, 0)
         #     else:
         #         output = torch.cat([output, torch.unsqueeze(h, 0)], 0)
-        x=torch.transpose(x,1,2)
+        x = torch.transpose(x, 1, 2)
         x = F.max_pool1d(x, x.size(2))
-        #print x.data.size()
-        x = self.linearOut(x.view(x.size(0),1,-1))
-        x = self.softmax(x.view(x.size(0),-1))
+        # print x.data.size()
+        x = self.dropout(x)
+        x = self.linearOut(x.view(x.size(0), 1, -1))
+        x = self.softmax(x.view(x.size(0), -1))
 
         return x
 
